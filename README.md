6ab405a027cbe33ea8b2bcbe4c5c1cf2353f8513
Andrew Revinsky <arevinsky@gmail.com>
2014-10-07 05:06:25 +0400

    Added automation and a few code lines

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
-inspigramdev
-============
+# Inspigramdev
 
-inspigramdev
+Inspigramdev
+
+## Getting Started
+Download the [production version][min] or the [development version][max].
+
+[min]: https://raw.github.com/AndrewRevinsky/inspigramdev/master/dist/inspigramdev.min.js
+[max]: https://raw.github.com/AndrewRevinsky/inspigramdev/master/dist/inspigramdev.js
+
+In your web page:
+
+```html
+<script src="jquery.js"></script>
+<script src="dist/inspigramdev.min.js"></script>
+<script>
+jQuery(function($) {
+  $.awesome(); // "awesome"
+});
+</script>
+```
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Release History
+_(Nothing yet)_
---
+++ static/js/redirect.js
@@ -0,0 +1,28 @@
+/**
+ * Created by ANDREW on 10/7/2014.
+ */
+$(function(){
+    var search = (function(input){
+        var result = {}, pairs = input.split(/\??(.[^&]*\&?)*/);
+        for(var item; item = pairs.shift(), typeof item != 'undefined'; ) {
+            if (!item) continue;
+            if (/=/.test(item)) {
+                var nv = item.split('=');
+                result[nv[0]] = nv[1];
+            }
+        }
+        return result;
+    })(window.location.search);
+
+    var auth = $.post('https://api.instagram.com/oauth/access_token', {
+        "client_id": "7c52051efa014bad915fe9bd29644358",
+        "client_secret": "d6972c67c4be458eb407f3f7d42017c5",
+        "grant_type": "authorization_code",
+        //"WEBSITE URL": "http://andrewrevinsky.github.io/inspigramdev/",
+        "redirect_uri":"http://andrewrevinsky.github.io/inspigramdev/redirect.html",
+        "code" : search.code
+    });
+
+
+
+});
